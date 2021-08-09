6f0a24e7d15ffe35282a466a63f4300f928c1df6
Lupita Navarro <williamsd80ror@gmail.com>
2021-08-09 16:11:44 -0500

    mensaje de cambios.

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -34,11 +34,14 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  "x" + "y" 
+  return 4
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  
 }
 
 function multiplica(x, y) {
